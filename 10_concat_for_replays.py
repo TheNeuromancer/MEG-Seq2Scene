32018ec8465253c583eeab17e13b6cc163de95d3
TheNeuromancer <desbordes.theo@gmail.com>
2025-01-14 17:33:27 +0100

    minor

--- 10_concat_for_replays.py
+++ 10_concat_for_replays.py
@@ -286,6 +286,7 @@ for iSub, sub in tqdm(enumerate(subs)):
     n_trials = len(trial_ids)
 
     for iLag in range(maxLag): # for each lag
+        if lag > 0: continue
         sf_all_trials, sb_all_trials, srand_all_trials = [], [], []
 
         for iTrial in range(n_trials):
